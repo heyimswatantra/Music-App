76a88758fc625bfc3864efd5a07dd0bf2604c5b9
heyimswatantra <swatantra.s.yadav@slrtce.in>
2024-05-05 10:49:38 +0530

    Add Contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,37 @@
 "use client"
+import { BackgroundBeams } from "@/components/ui/background-beams"
 
 export default function Page() {
     
 
     return (
-        <div>test 2</div>
+        <div className="h-[45rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+        <div className="max-w-4xl mx-auto p-4">
+          <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+            Contact Us
+          </h1>
+          <p></p>
+          <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+            We're here to help with any queries about our courses, programs, or events.
+            Reach out and let us know how we can assist you in your musical journey.
+          </p>
+          <input
+            type="text"
+            placeholder="hi@manuarora.in"
+            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
+          />
+          <div className="">
+          <input
+            type="text"
+            placeholder="Your message"
+            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
+          />
+          </div>
+          <p></p>
+          <button className="px-8 py-2 mt-4 rounded-md bg-teal-500 text-white font-bold transition duration-200 hover:bg-white hover:text-black border-2 border-transparent hover:border-teal-500">Invert it</button>
+        </div>
+        <BackgroundBeams />
+      </div>
+  
     )
 }
